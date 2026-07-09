c5cc0114275b723cbd35adbccffc799bae9c22d4
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-09 02:28:35 -0700

    Module4b - Fetch records from items table

--- show_inventory.php
+++ show_inventory.php
@@ -5,6 +5,9 @@ error_reporting(E_ALL);
 
 try {
     $db = new PDO("mysql:host=localhost;dbname=inventory_db", "root", "");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->query("SELECT * FROM items");
+    $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
