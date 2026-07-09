d3ed2c1c2ad1a068c215bf08eff2ce3fa22cee16
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-09 02:31:20 -0700

    Module4b - render inventory items as html

--- show_inventory.php
+++ show_inventory.php
@@ -6,8 +6,12 @@ error_reporting(E_ALL);
 try {
     $db = new PDO("mysql:host=localhost;dbname=inventory_db", "root", "");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    
     $stmt = $db->query("SELECT * FROM items");
     $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   
+    foreach ($items as $item) {
+        echo "<p>{$item['item_name']} ({$item['quantity']} units)</p>";
+    }
 }
 ?>
