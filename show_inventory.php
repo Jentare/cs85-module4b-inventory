c6ddd64e415fbc459abbd20eae265abd44904f3d
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-09 02:24:34 -0700

    Module4b - Establish PDO connection

--- show_inventory.php
+++ show_inventory.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 try {
-    
+    $db = new PDO("mysql:host=localhost;dbname=inventory_db", "root", "");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 }
 ?>
